3612c0b7b1e404a3e2bae8636d3dd24165000f69
Felipe Lahti <felipe.lahti@gmail.com>
2013-04-02 01:22:46 -0300

    Separating multiply matrix in a different file to get easy to test for unix process.

---
+++ multiply_matrices/unix_process/lib/multiply_matrix_process.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+Matrix multipleMatrix(Matrix m1, Matrix m2){
+	Matrix result;
+	result.width = m1.width;
+	result.height = m2.height;
+	allocMatrix(&result);
+
+	int i,j;
+	for (i = 0; i < result.width; ++i) {
+		for (j = 0; j < result.height; ++j) {
+			result.matrix[i][j] = multiplyLineColumn(i, j, m1, m2);
+		}
+	}
+
+	return result;
+}
--- multiply_matrices/unix_process/test/multiply_matrices_process_test.c
+++ multiply_matrices/unix_process/test/multiply_matrices_process_test.c
@@ -1,3 +1,4 @@
 #include "../../lib/matrix.c"
 #include "../../lib/test.c"
+#include "../lib/multiply_matrix_process.c"
 #include "../../test/multiply_matrices_template_test.c"
