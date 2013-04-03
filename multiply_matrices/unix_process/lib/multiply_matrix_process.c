c74ae07bcc7fa5d00b7be856d51e634cc3bea3cd
Felipe Lahti <felipe.lahti@gmail.com>
2013-04-02 23:17:16 -0300

    Added matrix multiplication using multi process. Work in progress(Need refactor).

--- multiply_matrices/unix_process/lib/multiply_matrix_process.c
+++ multiply_matrices/unix_process/lib/multiply_matrix_process.c
@@ -1,34 +1,106 @@
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
 #include <unistd.h>
 
-Matrix *sharedMatrix;
+typedef struct {
+	long type;
+	int result;	
+	int line;
+	int column;
+} LineColumnResult;
 
-Matrix multipleMatrix(Matrix m1, Matrix m2){
+key_t ipckeyAggregator, ipckeyProcess;
+int messageQueueIdAggregator, messageQueueIdProcess;
+
+#define TOTAL_PROCESS 2
+
+void init_line_column(LineColumnResult *variable, int line, int column) {
+	variable->type = 1;
+	variable->result = 0;
+	variable->line = line;
+	variable->column = column;
+}
+
+void printLineColumn(LineColumnResult p){
+	printf("Line: %d Column: %d Result: %d\n", p.line, p.column, p.result);	
+}
+
+void childProcess(Matrix m1, Matrix m2){
+
+	while(1) {
+		LineColumnResult temp;
+		int received = msgrcv(messageQueueIdProcess, &temp, sizeof(LineColumnResult), 1, IPC_NOWAIT);
+
+		if (received >= 0){
+			temp.result = multiplyLineColumn(temp.line, temp.column, m1, m2);
+			temp.type = 2;
+			msgsnd(messageQueueIdAggregator, &temp, sizeof(LineColumnResult), 0);
+		} else {
+			break;
+		}
+	}
+
+	exit(0);
+}
+
+Matrix prepareResultMatrix(Matrix m1, Matrix m2) {
 	Matrix result;
 	result.width = m1.width;
 	result.height = m2.height;
 	allocMatrix(&result);
+	return result;
+}
 
-	sharedMatrix = &result;
-
-	pid_t *process = calloc(result.width * result.height, sizeof(pid_t));
-	int status;
-
+Matrix multipleMatrix(Matrix m1, Matrix m2){
+	Matrix result = prepareResultMatrix(m1,m2);
 	int i,j;
+
+	ipckeyProcess = ftok(".", 'm');
+	messageQueueIdProcess = msgget(ipckeyProcess, IPC_CREAT | 0666);
+
+	ipckeyAggregator = ftok("/tmp/aggregator", 42);
+	messageQueueIdAggregator = msgget(ipckeyAggregator, IPC_CREAT | 0666);
+
 	for (i = 0; i < result.width; ++i) {
 		for (j = 0; j < result.height; ++j) {
-			pid_t id = process[i * result.width + j] = fork();			
-			if ( id == 0) {
-				result.matrix[i][j] = multiplyLineColumn(i, j, m1, m2);
-				printf("%d\n", result.matrix[i][j]);
-				exit(0);
-			} else {
-				//printf("%d\n", id);
-				//sleep(1);
+			LineColumnResult lineColumnResult;
+			init_line_column(&lineColumnResult, i, j);
+
+			msgsnd(messageQueueIdProcess, &lineColumnResult, sizeof(LineColumnResult), 0);
+		}
+	}
+
+	//Dispatching process
+	pid_t *process = calloc(TOTAL_PROCESS, sizeof(pid_t));
+
+	for (i = 0; i < TOTAL_PROCESS; ++i)	{
+		process[i] = fork();
+
+		if (process[i] == 0) {
+			printf("Process created!\n");
+			childProcess(m1, m2);
+		}
+	}
+
+	int totalCalls = result.width * result.height;
+
+	while(1) {
+		LineColumnResult temp2;
+		int received = msgrcv(messageQueueIdAggregator, &temp2, sizeof(LineColumnResult), 2, IPC_NOWAIT);
+		
+		if (received >= 0){			
+			printLineColumn(temp2);
+			result.matrix[temp2.line][temp2.column] = temp2.result;
+			totalCalls--;
+		} else {
+			if(totalCalls <= 0) {
+				break;
 			}
 		}
 	}
 
-	for (i = 0; i < result.width * result.height; ++i) {
+	for (i = 0; i < TOTAL_PROCESS; ++i) {
 		wait(&process[i]);
 	}
 
