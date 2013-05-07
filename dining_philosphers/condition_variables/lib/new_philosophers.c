3d05c6a6e8facbb27e5f027c473fa2b18cf44cc7
Eduardo Potter <epotter@ionatec.com.br>
2013-05-06 23:36:36 -0300

    comentado agora

--- dining_philosphers/condition_variables/lib/new_philosophers.c
+++ dining_philosphers/condition_variables/lib/new_philosophers.c
@@ -12,12 +12,12 @@ int *state;
 int philosophers;
 int *right_hungry;
 int *left_hungry; //this is to avoid starvation
+//o algoritmo usado foi o que esta no livro do silberchatz http://www.google.de/url?sa=t&rct=j&q=operating%20system%20concepts%20pdf&source=web&cd=1&ved=0CDMQFjAA&url=http%3A%2F%2Fiit.qau.edu.pk%2Fbooks%2FOS_8th_Edition.pdf&ei=1T2IUY7YJe-w4APr94HYAQ&usg=AFQjCNHJuAZTm-YxdmXzTEW5jwNLk7Rbdw&cad=rja
 
-void changeStateAndPrint (int philosopherNumber, int newState){
-	//pthread_mutex_lock(&printing);	
+//url acima se tu qusier dar uma olhada, tem na definicao do trabalho aonde olhar
+void changeStateAndPrint (int philosopherNumber, int newState){	//gambiarra pra setar um estado e imprimir todos os filosofos
 	int i;
 	state[philosopherNumber] = newState;
-	//printf("this is :%d", philosopherNumber);
 	for (i=0;i<philosophers;i++){
 		if (state[i] == EATING )
 			printf("E ");
@@ -27,13 +27,9 @@ void changeStateAndPrint (int philosopherNumber, int newState){
 			printf("T ");	
 	}
 	printf("\n");
-	//pthread_mutex_unlock(&printing);	
-	//printf("aaa\nss");
 }
 
-void test(int i) {
-	//printf("test %d \n", i);
-	//printf("passou test%d \n", i);
+void test(int i) { // como test sempre e chamado por pickup ou putdown, que ja estao no mutex, nao precisamos mutex aqui...
 	if ((state[(i + philosophers - 1) % philosophers] !=EATING) && (state[i] ==HUNGRY) && (state[(i + 1) % philosophers] !=EATING) && !right_hungry[i] && !left_hungry[i] ) {
 		changeStateAndPrint(i,EATING);
 		pthread_cond_signal(&cond_state[i]);
@@ -42,7 +38,7 @@ void test(int i) {
 }
 
 
-void pickup(int i) {
+void pickup(int i) { //o deadlock e evitado pois o filosofo pega os dois talheres de forma exclusiva
 	pthread_mutex_lock(&m);
 	//printf("pickup%d \n", i);
 	changeStateAndPrint(i,HUNGRY);
@@ -52,12 +48,12 @@ void pickup(int i) {
         left_hungry[(i + philosophers - 1) % philosophers] = 0;	
 	pthread_mutex_unlock(&m);			
 }
-void putdown(int i) {
+void putdown(int i) { 
 	pthread_mutex_lock(&m);
 	changeStateAndPrint(i,THINKING);
 	test((i + philosophers - 1) % philosophers);
 	if (state[(i + philosophers - 1) % philosophers] == HUNGRY)
-		left_hungry[i] = 1;
+		left_hungry[i] = 1; //serve para evitar a starvation, ou seja, se quando devolvemos o talher, o vizinho quer comer, damos a prioridade pra ele, pra evitar que alguem fique comendo como um gordo indefinidamente e cometa starvation
 	test((i + 1) % philosophers);
 	if (state[(i + 1) % philosophers])
 		right_hungry[i] = 1;
@@ -73,7 +69,6 @@ void *philosopherThread(void *ptr) {
 	putdown(philosopherNumber);
 	sleepTime = rand() % 10 + 1;
 	sleep(sleepTime);
-	//printf("%d finished eating\n",philosopherNumber);
      	
     }
     pthread_exit(0);
@@ -82,22 +77,21 @@ void *philosopherThread(void *ptr) {
 void philosophersUsingSemaphores( int numberOfThreads) {
     philosophers = numberOfThreads;
     pthread_t *threads = calloc(philosophers, sizeof(sizeof(pthread_t)));
-    cond_state = calloc(philosophers,sizeof(pthread_cond_t));
+    cond_state = calloc(philosophers,sizeof(pthread_cond_t)); //variaveis de condicao
     state = calloc(philosophers,sizeof(int));
-    left_hungry = calloc(philosophers,sizeof(int));
+    left_hungry = calloc(philosophers,sizeof(int)); //para starvation
     right_hungry = calloc(philosophers,sizeof(int));
     int i, *argsAux;
-    pthread_mutex_init(&m, NULL);
-    pthread_mutex_init(&printing, NULL);
+    pthread_mutex_init(&m, NULL); //mutex para simular monitor
     for(i=0;i<philosophers;i++) {
 	left_hungry[i] = 0;
 	right_hungry[i] = 0;
-	pthread_cond_init(&cond_state[i], NULL);
+	pthread_cond_init(&cond_state[i], NULL);  //inicializa conditions variables, e todos estao pensando
 	state[i] = THINKING;    
     }   
     for (i = 0; i < philosophers; i++) {
 	argsAux[i] = i;        
-        pthread_create(&threads[i], NULL, philosopherThread, (void *) &argsAux[i]);
+        pthread_create(&threads[i], NULL, philosopherThread, (void *) &argsAux[i]); //cria philosphers threads
     }
 	
     for(i=0;i<philosophers;i++) {
